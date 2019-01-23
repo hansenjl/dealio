9c4880311078b8926e22018d881037491f9895ca
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2019-01-23 17:47:03 +0000

    add choose category method

--- lib/dealio/cli.rb
+++ lib/dealio/cli.rb
@@ -15,6 +15,7 @@ class Dealio::CLI
         puts "in electronics"
         scrape_electronics
         list_categories
+        choose_category
       when "home"
          puts "in home"
       when "shoes"
@@ -23,7 +24,7 @@ class Dealio::CLI
          puts "Goodbye"
       else
         puts "Sorry! I didn't understand that input"
-        menu
+        menu  #recursion 
     end
   end
   
@@ -31,6 +32,25 @@ class Dealio::CLI
     Dealio::Category.all.each.with_index(1) do |category, index|
       puts "#{index}. #{category.name}"
     end
+    
+  end
+  
+  def choose_category
+    puts "\nChoose a category by selecting a number:"
+    input = gets.strip.to_i 
+    max_value = Dealio::Category.all.length 
+    if input.between?(1,max_value)
+      category = Dealio::Category.all[input-1]
+      display_category_items(category)
+    else
+      puts "\nPlease put in a valid input"
+      list_categories
+      choose_category
+    end
+  end
+  
+  def display_category_items(category)
+    
   end
   
   def scrape_electronics
